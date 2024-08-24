519c621f51eda621ad97cd365bc0c9e78030733b
LBaldassarre <lbaldassarre.dev@gmail.com>
2024-08-24 12:53:47 +0200

    Add router and folder structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+
+import Home from './pages/Home/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
